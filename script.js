b11682ea994f48e87174fcbd74b393d50334ce76
Pooya Gordani <pooyagordani24@gmail.com>
2025-03-10 20:36:42 +0330

    set a variable name for checkbox in js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.getElementById('check').addEventListener('change', function() {
+const checkBox_agree_to_terms = document.getElementById('check');
+checkBox_agree_to_terms.addEventListener('change', function() {
     document.getElementById('btn-sign-up').disabled = !this.checked;
 });
